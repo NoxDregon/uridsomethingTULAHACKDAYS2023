0c46855c9390fc87fd77dae7041f50cf91401ae0
NoxDregon <74489530+NoxDregon@users.noreply.github.com>
2023-12-03 00:28:53 +0300

    hehe, boy

--- src/components/futer.js
+++ src/components/futer.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import '../index.css';
-
 import Image from "./Image";
 import VK from "../image/icons/vk.png"
 import TG from "../image/icons/tg.png"
 import EMAIL from "../image/icons/email.png"
 import POINT from "../image/icons/point.png"
+import EMAILB from "../image/icons/email_black.png"
+import POINTB from "../image/icons/point_black.png"
 
 function Futer() {
     return (
@@ -18,7 +19,7 @@ function Futer() {
             </div>
             <div className="WeebsBlock">
                 <Image image = {TG} className="WeebsIcon"/>
-                <p className="WeebsText"> ·Телеграмм</p>
+                <p className="WeebsText"> ·Телеграм</p>
             </div>
             <div className="WeebsBlock">
                 <Image image = {EMAIL} className = "WeebsIcon"/>
@@ -33,8 +34,27 @@ function Futer() {
               <button className="MenuButton">Поддержка</button>
               <div className="Arow">↓</div>
           </div>
+          <div className='downmenu'>
+              <div>
+                <div className="partOfDownMenu">
+                  <Image image = {EMAILB} className = "WeebsIcon"/>
+                  <p className="WeebsText"> ·xakdays@gmail.com</p>
+                </div>
+                <div className="partOfDownMenu">
+                  <Image image = {TG} className="WeebsIcon"/>
+                  <p className="WeebsText"> ·Телеграм</p>
+                </div>
+                <div className="partOfDownMenu">
+                  <Image image = {VK} className = "WeebsIcon"/>
+                  <p className="WeebsText"> ·Вконтанте</p>
+                </div> 
+                <div className="partOfDownMenu">
+                <Image image = {POINTB} className = "WeebsIcon"/>
+                <p className="WeebsText">· к, пр. Ленина, 125 корпус 3, Тула, Тульская обл., 300026</p>
+                </div> 
+              </div>
+          </div>
       </div>
     );
   }
-  
   export default Futer;
--- src/components/header.js
+++ src/components/header.js
@@ -24,5 +24,6 @@ function Header() {
         </header>
      
     );
+    
   }
   export default Header;
