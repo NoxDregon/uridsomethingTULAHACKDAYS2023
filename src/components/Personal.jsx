d5f13ad82fe4254394c4332e8ee99bb1690053d0
fROSTY1L <124553997+fROSTY1L@users.noreply.github.com>
2023-12-03 13:14:48 +0300

    Comish

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -8,21 +8,24 @@ const Personal = () => {
             <div className="personal-container">
                 <div className="profilePhoto"></div>
                 <div className="profile-info">
-                    <div className="profile-info__name">Фамилия Имя Отчество</div>
+                    <div className="profile-info__name"><input className="profile-input" type="text" placeholder="Введите ФИО"/></div>
                     <div className="profile-info__documents">
+                        <form action="">
                         <ol className="ol-profile">
-                            <li>Паспортные данные</li>
-                            <li>СНИЛС</li>
-                            <li>ИНН</li>
-                            <li>Адрес проживания</li>
-                            <li>Кадастровый номер участка</li>
-                            <li>Номер счета</li>
-                            <li>ОРГН</li>
-                            <li>Номер статьи договора</li>
-                            <li>Сумма долга у арендатора</li>
-                            <li>Сумма начисления пени у арендатора</li>
-                            <li>Дата заключения / окончания договора</li>
+                            <li><input className="profile-input" type="text" placeholder="Паспортные данные"/></li>
+                            <li><input className="profile-input" type="text" placeholder="ИНН"/></li>
+                            <li><input className="profile-input" type="text" placeholder="Адрес проживания"/></li>
+                            <li><input className="profile-input" type="text" placeholder="Кадастровый номер участка"/></li>
+                            <li><input className="profile-input" type="text" placeholder="Номер счета"/></li>
+                            <li><input className="profile-input" type="text" placeholder="ОРГН"/></li>
+                            <li><input className="profile-input" type="text" placeholder="Номер статьи договора"/></li>
+                            <li><input className="profile-input" type="text" placeholder="Сумма долга арендатора у плательщика"/></li>
+                            <li><input className="profile-input" type="text" placeholder="Сумма начисления пени у арендатора"/></li>
+                            <li> <div className="li-block">Дата заключения договора<input className="profile-input" type="date" placeholder="Дата заключения / окончания договора"/></div></li>
+                            <li> <div className="li-block">Дата заключения договора<input className="profile-input" type="date" placeholder="Дата заключения / окончания договора"/></div></li>
                         </ol>
+                        <input type="submit" value="Отправить форму"/>
+                        </form>
                     </div>
                 </div>
             </div>
